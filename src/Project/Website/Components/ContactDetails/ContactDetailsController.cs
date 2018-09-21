690e0678da93cb713186f87149e3bbf96058e677
Mark Cassidy <mark@cassidy.dk>
2018-09-21 15:04:21 +0200

    Contact Details with Map done

--- src/Project/Website/Components/ContactDetails/ContactDetailsController.cs
+++ src/Project/Website/Components/ContactDetails/ContactDetailsController.cs
@@ -19,7 +19,13 @@ namespace Project.Website.Components.ContactDetails
 
 		protected virtual ContactDetailsModel GetModel(Item actionItem)
 		{
-			return new ContactDetailsModel();
+			return new ContactDetailsModel
+			{
+				Latitude = actionItem["Contact Details with Map Latitude"],
+				Longitude = actionItem["Contact Details with Map Longitude"],
+				Span = actionItem["Contact Details with Map Span"],
+				Text = RenderField(actionItem, "Contact Details with Map Text"),
+			};
 		}
 	}
 }
--- src/Project/Website/Components/ContactDetails/ContactDetailsModel.cs
+++ src/Project/Website/Components/ContactDetails/ContactDetailsModel.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 
 namespace Project.Website.Components.ContactDetails
 {
 	public class ContactDetailsModel : ComponentModel
 	{
+		public string Latitude { get; set; }
+		public string Longitude { get; set; }
+		public string Span { get; set; }
+		public HtmlString Text { get; set; }
 	}
 }
